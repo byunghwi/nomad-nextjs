72890371b9cc6aab0c03f6acc64f924c85e28b46
고병휘 <gpdldy91@gmail.com>
2025-08-11 22:43:12 +0900

    add constants

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
-import { API_URL } from '../app/(home)/page';
+
+import { API_URL } from '../constants';
 import styles from '../styles/movie-info.module.css';
 
 export const getMovieDetail = async (id: string) => {
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
-import { API_URL } from '../app/(home)/page';
+
+import { API_URL } from '../constants';
 import styles from '../styles/movie-videos.module.css';
 
 const getVideos = async (id: string) => {
